818b8e4b190a0edc0675f9de74d5a0f2d70ecaac
Wen Jin <Nijnxw@users.noreply.github.com>
2021-06-10 20:07:16 +0800

    misc: clear CI warnings

--- src/components/commons/ProgressBar.js
+++ src/components/commons/ProgressBar.js
@@ -1,26 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 const ProgressBar = ({ value = 0, containerStyle = {} }) => {
   const [style, setStyle] = useState({});
   const [styleValue, setStyleValue] = useState({});
 
-  const animation = () => {  
-    setTimeout(() => {
-      const newStyle = {
-          opacity: 1,
-          width: `${value}%`,
-          background: value < 30 ? '#FF9999' : (value < 75 ? '#FFBB77' : '#6CBA95')
-      }
-      setStyleValue({
+  setTimeout(() => {
+    const newStyle = {
         opacity: 1,
-      })
-      setStyle(newStyle);
-    }, 500);
-  }
-
-  useEffect(() => {
-    animation()
-  }, [value])
+        width: `${value}%`,
+        background: value < 30 ? '#FF9999' : (value < 75 ? '#FFBB77' : '#6CBA95')
+    }
+    setStyleValue({
+      opacity: 1,
+    })
+    setStyle(newStyle);
+  }, 500);
   
   return (
     <div className="progress-container">
